9ab7a0a6c251d6376a4297b4ce59522714f1344d
hilu <nikanozadze1010@gmail.com>
2022-11-19 21:14:19 +0400

    feat: work with birth modal

--- scripts/map.js
+++ scripts/map.js
@@ -1,4 +1,27 @@
 const descriptions = document.querySelectorAll(".descriptions-list-item");
+const addressSelect = document.querySelector("#birth-address");
+const modal = document.querySelector(".modal");
+const closeIcon = document.querySelector(".close");
+const mapForm = document.querySelector("#birth-form");
+
+addressSelect.value = "";
+addressSelect.addEventListener("change", () => {
+  addressSelect.style.color = "#000000";
+});
+
+const closeModal = () => {
+  modal.style.display = "none";
+};
+
+closeIcon.addEventListener("click", () => {
+  closeModal();
+});
+
+mapForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  closeModal();
+});
+
 let activeDescription;
 
 Array.from(descriptions).map((description) => {
