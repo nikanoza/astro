3361b5c25e7f4b0941c566756e83f2ecca14d81f
hilu <nikanozadze1010@gmail.com>
2022-12-16 22:23:14 +0400

    feat: work with registration modal

--- scripts/map.js
+++ scripts/map.js
@@ -3,6 +3,16 @@ const addressSelect = document.querySelector("#birth-address");
 const modal = document.querySelector(".modal");
 const closeIcon = document.querySelector(".close");
 const mapForm = document.querySelector("#birth-form");
+const mapPopUp = document.querySelector(".map-popup");
+const loading = document.querySelector(".loading");
+const closeRegModal = document.getElementById("close-reg-btn");
+const regPopupForm = document.querySelector("#reg-form");
+const regPopupNameBox = document.getElementById("reg-name");
+const regNameInput = document.getElementById("reg-name-input");
+const regUserIcon = document.querySelector(".reg-user-icon");
+const passwordIcon = document.getElementById("eye-icon-password");
+const repeatPasswordIcon = document.getElementById("eye-icon-repeat-password");
+const confirm = document.querySelector(".confirm-checkbox");
 
 addressSelect.value = "";
 addressSelect.addEventListener("change", () => {
@@ -17,9 +27,42 @@ closeIcon.addEventListener("click", () => {
   closeModal();
 });
 
+const openRegisterModal = () => {
+  const registerPopup = document.getElementById("register-popup");
+  registerPopup.style.display = "flex";
+};
+
+closeRegModal.addEventListener("click", () => {
+  const registerPopup = document.getElementById("register-popup");
+  registerPopup.style.display = "none";
+});
+
 mapForm.addEventListener("submit", (event) => {
   event.preventDefault();
-  closeModal();
+  mapPopUp.style.display = "none";
+  loading.style.display = "flex";
+  setTimeout(() => {
+    const circle = document.getElementById("popup-circle");
+    const logo = document.getElementById("popup-logo");
+    const dotedCircle = document.getElementById("popup-doted-circle");
+    const checked = document.getElementById("popup-checked");
+    const text = document.getElementById("popup-text");
+    const description = document.getElementById("popup-description");
+
+    circle.style.display = "none";
+    logo.style.display = "none";
+    dotedCircle.style.display = "block";
+    checked.style.display = "block";
+
+    text.textContent = "თქვენი მონაცემების დამუშავება წარმატებით დასრულდა";
+    description.style.display = "none";
+  }, 3000);
+  setTimeout(() => {
+    closeModal();
+  }, 5000);
+  setTimeout(() => {
+    openRegisterModal();
+  }, 8000);
 });
 
 let activeDescription;
@@ -36,4 +79,43 @@ Array.from(descriptions).map((description) => {
       activeDescription.classList.toggle("active");
     }
   });
+});
+
+regNameInput.addEventListener("input", (event) => {
+  console.log(1);
+  if (event.target.value.trim().length > 0) {
+    console.log(2);
+    regUserIcon.children[0].style.fill = "#634aae";
+  } else {
+    console.log(3);
+    regUserIcon.children[0].style.fill = "#44487d";
+  }
+});
+
+passwordIcon.addEventListener("click", () => {
+  const password = document.getElementById("reg-password-input");
+  if (password.type === "password") {
+    password.type = "text";
+  } else {
+    password.type = "password";
+  }
+});
+
+repeatPasswordIcon.addEventListener("click", () => {
+  const password = document.getElementById("reg-repeat-password-input");
+  if (password.type === "password") {
+    password.type = "text";
+  } else {
+    password.type = "password";
+  }
+});
+
+confirm.addEventListener("click", () => {
+  confirm.classList.toggle("active");
+});
+
+regPopupForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const registerPopup = document.getElementById("register-popup");
+  registerPopup.style.display = "none";
 });
